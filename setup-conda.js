4e6ac751fa32e638de234b7e50c6c497c342921d
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2019-08-28 18:02:17 +0900

    definitions of and prior to __importStar()
    https://github.com/actions/setup-python

--- setup-conda.js
+++ setup-conda.js
@@ -1,5 +1,22 @@
 // https://www.tizag.com/javascriptT/javascriptcomment.php
-
+// https://github.com/actions/setup-python
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"))
 const shell = require('shelljs')
 
